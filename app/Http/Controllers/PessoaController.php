3753182ad1c398caa038f6680854dfdbd6c1d289
Rafael Maia <rafael7maia@gmail.com>
2017-05-10 15:17:45 -0300

    learning lumen

--- app/Http/Controllers/PessoaController.php
+++ app/Http/Controllers/PessoaController.php
@@ -1,12 +1,14 @@
 <?php
 
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Resquest;
 
+
 class PessoaController extends Controller
 {
-   public function cadastrar(Request $request){
+   public function cadastrar(Resquest $request){
 
    		return view("verPessoa", [
    			'nome' => $request->input('nome'),
